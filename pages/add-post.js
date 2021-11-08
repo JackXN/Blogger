0cb2fb8af0f0ee501d1a83a9340ec067f80ed138
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-11-07 20:15:12 -0700

    Updated add-post.js

--- pages/add-post.js
+++ pages/add-post.js
@@ -1,70 +1,91 @@
-import Nav from "../components/Nav";
-import {useState} from 'react';
+import { useState } from 'react';
+
+import Nav from '../components/Nav';
 import styles from '../styles/Home.module.css';
-import {Box, Button} from '@chakra-ui/react';
 
-const AddPost = () => {
-const [title, setTitle] = useState('');
-const [content, setContent] = useState('');
-const [error,setError] = useState('');
-const [message, setMessage] = useState('');
+export default function AddPost() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
 
+    const handlePost = async (e) => {
+        e.preventDefault();
 
-const handlePost = async (e) => {
-    e.preventDefault();
+        // reset error and message
+        setError('');
+        setMessage('');
 
-    //Reset the error and message
-setError('');
-setMessage('');
+        // fields check
+        if (!title || !content) return setError('All fields are required');
 
-// Field Check
-if(!title || !content) return setError('All fields are required')
-};
+        // post structure
+        let post = {
+            title,
+            content,
+            published: false,
+            createdAt: new Date().toISOString(),
+        };
+        // save the post
+        let response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify(post),
+        });
 
-return (
-    <>
-    <Box>
-        <Nav/>
-        <Box className={styles.container}>
-        <form onSubmit={handlePost} className={styles.form}>
-            {error ? (
-                <Box className={styles.formItem}>
-                    <h3 className={styles.error}>{error}</h3>
-                </Box>
-            ) : null} 
-            {message ? (
-                <Box className={styles.formItem}>
-                    <h3 className={styles.message}>{message}</h3>
-                </Box>
-            ) : null}
-            <Box className={styles.formItem}>
-                <label>Title</label>
-                <input
-                type={text}
-                name='title'
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                placeholder='title'
-                />
-            </Box>
-            <Box className={styles.formItem}>
-                <label>Content</label>
-                <textarea
-                name='content'
-                onChange={(e) => setContent(e.target.value)}
-                placeholder='Post Content'
-                value={content}
-                ></textarea>
-            </Box>
-            <Box className={styles.formItem}>
-                <Button type='submit'>Add Post</Button>
-            </Box>
-        </form>
-        </Box>
-    </Box>
-    </>
-)
+        // get the data
+        let data = await response.json();
 
+        if (data.success) {
+            // reset the fields
+            setTitle('');
+            setContent('');
+            // set the message
+            return setMessage(data.message);
+        } else {
+            // set the error
+            return setError(data.message);
+        }
+    };
+
+    return (
+        <div>
+            <Nav />
+            <div className={styles.container}>
+                <form onSubmit={handlePost} className={styles.form}>
+                    {error ? (
+                        <div className={styles.formItem}>
+                            <h3 className={styles.error}>{error}</h3>
+                        </div>
+                    ) : null}
+                    {message ? (
+                        <div className={styles.formItem}>
+                            <h3 className={styles.message}>{message}</h3>
+                        </div>
+                    ) : null}
+                    <div className={styles.formItem}>
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                            placeholder="title"
+                        />
+                    </div>
+                    <div className={styles.formItem}>
+                        <label>Content</label>
+                        <textarea
+                            name="content"
+                            onChange={(e) => setContent(e.target.value)}
+                            value={content}
+                            placeholder="Post content"
+                        />
+                    </div>
+                    <div className={styles.formItem}>
+                        <button type="submit">Add post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
-
-export default AddPost;
